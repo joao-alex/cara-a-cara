a53b8e54e13239c8f2fdfe791412bd39800e2a2d
Joao Alex <joao_alex2000@hotmail.com>
2020-05-03 22:48:34 -0300

    Updates

---
+++ precache-manifest.3dfd82a0998b2fccf26f7a9e65ab0847.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "717bb8391c16d1aa45dcd204c917aeb8",
+    "url": "/cara-a-cara/index.html"
+  },
+  {
+    "revision": "72493a0e133ab71c90b8",
+    "url": "/cara-a-cara/static/css/main.4c55b77a.chunk.css"
+  },
+  {
+    "revision": "438e25b2a2adeefa8983",
+    "url": "/cara-a-cara/static/js/2.4c79f77e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cara-a-cara/static/js/2.4c79f77e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72493a0e133ab71c90b8",
+    "url": "/cara-a-cara/static/js/main.7b4eaf1a.chunk.js"
+  },
+  {
+    "revision": "6c5990906ffc78e3ad88",
+    "url": "/cara-a-cara/static/js/runtime-main.6a9e861b.js"
+  },
+  {
+    "revision": "90729f8823421e0c709b7a56be5ee3b3",
+    "url": "/cara-a-cara/static/media/1.90729f88.png"
+  },
+  {
+    "revision": "6998b58560d8db39cb83599fde235d7f",
+    "url": "/cara-a-cara/static/media/10.6998b585.png"
+  },
+  {
+    "revision": "50e8c28e13d03694e46632dbd4792250",
+    "url": "/cara-a-cara/static/media/11.50e8c28e.png"
+  },
+  {
+    "revision": "b91d152cd6f8a1de8d7a6f1eaaa3952c",
+    "url": "/cara-a-cara/static/media/12.b91d152c.png"
+  },
+  {
+    "revision": "58088b200a1a9f7c24d0425192fac38c",
+    "url": "/cara-a-cara/static/media/13.58088b20.png"
+  },
+  {
+    "revision": "66478163e465c6192350d7b7aa966313",
+    "url": "/cara-a-cara/static/media/14.66478163.png"
+  },
+  {
+    "revision": "b8cfc1acbe542abfb3c055def7278266",
+    "url": "/cara-a-cara/static/media/15.b8cfc1ac.png"
+  },
+  {
+    "revision": "80c094b97c98efdf9f58b5fbefd6c8dc",
+    "url": "/cara-a-cara/static/media/16.80c094b9.png"
+  },
+  {
+    "revision": "25d9f9534b24f53b673d2555120246e6",
+    "url": "/cara-a-cara/static/media/17.25d9f953.png"
+  },
+  {
+    "revision": "2202c3dff00c9e8060761cd32ab3f1ff",
+    "url": "/cara-a-cara/static/media/18.2202c3df.png"
+  },
+  {
+    "revision": "350e7bb0ecf13e3fc99305c7932fe42c",
+    "url": "/cara-a-cara/static/media/19.350e7bb0.png"
+  },
+  {
+    "revision": "f7daf0fc60d8eb84995e2b0227af63d6",
+    "url": "/cara-a-cara/static/media/2.f7daf0fc.png"
+  },
+  {
+    "revision": "c15972b7e62107fcfa4cac852cee5b9e",
+    "url": "/cara-a-cara/static/media/20.c15972b7.png"
+  },
+  {
+    "revision": "f6c0034647c4f9b3bb3d7a3cd39352ec",
+    "url": "/cara-a-cara/static/media/21.f6c00346.png"
+  },
+  {
+    "revision": "2685caff213715196c0a7e37f5bb2c3f",
+    "url": "/cara-a-cara/static/media/3.2685caff.png"
+  },
+  {
+    "revision": "f8b4cbf5fc0922da296d57f2550f86fd",
+    "url": "/cara-a-cara/static/media/4.f8b4cbf5.png"
+  },
+  {
+    "revision": "32b2f010045e2aa91a1c08fe49a1ed32",
+    "url": "/cara-a-cara/static/media/5.32b2f010.png"
+  },
+  {
+    "revision": "d52d24aa1b395efe9dd97dccca8b2763",
+    "url": "/cara-a-cara/static/media/6.d52d24aa.png"
+  },
+  {
+    "revision": "3139f180908b4f40be54446520988c70",
+    "url": "/cara-a-cara/static/media/7.3139f180.png"
+  },
+  {
+    "revision": "5f2e4ced4e2fea7448f7138005d9024b",
+    "url": "/cara-a-cara/static/media/8.5f2e4ced.png"
+  },
+  {
+    "revision": "e11e72369ffefc44710c5a42a70540b6",
+    "url": "/cara-a-cara/static/media/9.e11e7236.png"
+  }
+]);
